5aa1edc7f54404c37c744ced525fbe9cff5a942c
amarilis <amarilispequeno@gmail.com>
2023-05-10 19:01:39 -0300

    semana 01 exercicio 01

---
+++ calculadora.js
@@ -0,0 +1,34 @@
+function somar() {
+  var num1 = parseFloat(document.getElementById("num1").value);
+  var num2 = parseFloat(document.getElementById("num2").value);
+  var resultado = num1 + num2;
+  document.getElementById("resultado").value = resultado;
+}
+
+function subtrair() {
+  var num1 = parseFloat(document.getElementById("num1").value);
+  var num2 = parseFloat(document.getElementById("num2").value);
+  var resultado = num1 - num2;
+  document.getElementById("resultado").value = resultado;
+}
+
+function multiplicar() {
+  var num1 = parseFloat(document.getElementById("num1").value);
+  var num2 = parseFloat(document.getElementById("num2").value);
+  var resultado = num1 * num2;
+  document.getElementById("resultado").value = resultado;
+}
+
+function dividir() {
+  var num1 = parseFloat(document.getElementById("num1").value);
+  var num2 = parseFloat(document.getElementById("num2").value);
+  var resultado = num1 / num2;
+  document.getElementById("resultado").value = resultado;
+}
+
+module.exports = {
+  somar,
+  multiplicar,
+  subtrair,
+  dividir,
+};
